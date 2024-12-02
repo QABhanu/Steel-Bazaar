3c316ba6eaf0e3a2748c4d774de7266d637f2c6d
QABhanu <bhanupratap.sah@abainfotech.com>
2024-12-02 14:33:10 +0530

    homepagechange

--- src/main/java/com/avighna/qa/pages/Homepage.java
+++ src/main/java/com/avighna/qa/pages/Homepage.java
@@ -1,5 +1,7 @@
 package com.avighna.qa.pages;
 
+import java.util.Locale.Category;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -24,11 +26,33 @@ public class Homepage{
     @FindBy(xpath = "//img[@class='cursor-pointer' and @src='/static/media/footerLogo.0b4b88fa279567e1afede2ba2f373af4.svg']")
     private WebElement steelbazaarIcon;
 
-    @FindBy(xpath = "//img[@class='cursor-pointer' and @src='/static/media/footerLogo.0b4b88fa279567e1afede2ba2f373af4.svg']")
-    private WebElement Categories;
+    @FindBy(xpath = "//*[@id=\"main\"]/div[1]/div[2]/div/div[1]/div[1]")
+    private WebElement DropDown;
 
-    @FindBy(xpath = "(//button[normalize-space(text())='View All'])[1]")
-    private WebElement viewallbutton;
+    @FindBy(xpath = "//a[text()='Raw / Semi Finished']")
+    private WebElement CategoriesName;
+
+    @FindBy(xpath = "//a[text()='Carbon Flat Rolled']")
+    private WebElement SuperCategoriesName;
+
+    @FindBy(xpath = "//a[text()='Hot-Rolled']")
+    private WebElement MainCategoriesName;
+
+    @FindBy(xpath = "//span[text()='Hot Rolled']")
+    private WebElement CategoryNameOnDropDown;
+
+    @FindBy(xpath = "//img[@alt='Hot Rolled']")
+    private WebElement ProductCategoriesName;
+
+    @FindBy(xpath = "//div[text()='Hot Rolled']")
+    private WebElement CategoriesNameonPLP;
+
+
+   
+
+
+
+
 
      
     public void search(String searchText) throws Throwable {
@@ -47,15 +71,52 @@ public class Homepage{
 
        steelbazaarIcon.click();
 
-       viewallbutton.click();
+
+       if (DropDown.isDisplayed() && DropDown.isEnabled()) {
+        DropDown.click();
+       }
 
 
+
+       if (CategoriesName.isDisplayed() && CategoriesName.isEnabled()) {
+        CategoriesName.click();
         
        }
 
+
+       if (SuperCategoriesName.isDisplayed() && SuperCategoriesName.isEnabled()) {
+        SuperCategoriesName.click();
+        
+       }
+
+       if (MainCategoriesName.isDisplayed() && MainCategoriesName.isEnabled()) {
+
+        MainCategoriesName.click();
+        
+       }
+
+       if (CategoryNameOnDropDown.isDisplayed() && CategoryNameOnDropDown.isEnabled()) {
+        CategoryNameOnDropDown.click();
+        String categoryNameFromDropDown = CategoryNameOnDropDown.getText(); // Get the text of the dropdown category
+        System.out.println("Category Name from Dropdown: " + categoryNameFromDropDown);
+
+       if (ProductCategoriesName.isDisplayed() && ProductCategoriesName.isEnabled()) {
+        ProductCategoriesName.click();
+       }
+
+
+       CategoriesNameonPLP.click();
+       String categoryNameFromPLP = CategoriesNameonPLP.getText();
+
+      }
+    }
+  }
+
+        
+      
+
+       
        
 	
-		
-	}
 
 
