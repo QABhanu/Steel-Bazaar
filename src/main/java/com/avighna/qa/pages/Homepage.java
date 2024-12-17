f40d4dca0aa0b186d9f236f6ac14c5325da51312
QABhanu <bhanupratap.sah@abainfotech.com>
2024-12-17 10:18:43 +0530

    changes

--- src/main/java/com/avighna/qa/pages/Homepage.java
+++ src/main/java/com/avighna/qa/pages/Homepage.java
@@ -20,7 +20,7 @@ public class Homepage{
     @FindBy(xpath = "(//input[@placeholder='Search anything in steel...'])[1]")
     private WebElement cataloguesearchfield;
      
-    @FindBy(xpath = "//li[text()='tata Hot Rolled Single length coil 10']")
+    @FindBy(xpath = "//li[text()='CRNO catalogue 8']")
     private WebElement cataloguefind;
      
     @FindBy(xpath = "//img[@class='cursor-pointer' and @src='/static/media/footerLogo.0b4b88fa279567e1afede2ba2f373af4.svg']")
@@ -41,11 +41,7 @@ public class Homepage{
     @FindBy(xpath = "//span[text()='Hot Rolled']")
     private WebElement CategoryNameOnDropDown;
 
-    @FindBy(xpath = "//img[@alt='Hot Rolled']")
-    private WebElement ProductCategoriesName;
-
-    @FindBy(xpath = "//div[text()='Hot Rolled']")
-    private WebElement CategoriesNameonPLP;
+    
 
 
    
@@ -58,7 +54,7 @@ public class Homepage{
     public void search(String searchText) throws Throwable {
 		cataloguesearchfield.click();
 		
-		cataloguesearchfield.sendKeys("tata Hot Rolled Single length coil 10");
+		cataloguesearchfield.sendKeys("CRNO catalogue 8");
 
     
        if (cataloguefind != null){
@@ -100,14 +96,7 @@ public class Homepage{
         String categoryNameFromDropDown = CategoryNameOnDropDown.getText(); // Get the text of the dropdown category
         System.out.println("Category Name from Dropdown: " + categoryNameFromDropDown);
 
-       if (ProductCategoriesName.isDisplayed() && ProductCategoriesName.isEnabled()) {
-        ProductCategoriesName.click();
-       }
-
-
-       CategoriesNameonPLP.click();
-       String categoryNameFromPLP = CategoriesNameonPLP.getText();
-
+       
       }
     }
   }
